6c1e728e121d37524f38ede5ddb2070479371e63
Scott Milton <miltons@onid.oregonstate.edu>
2015-02-11 22:09:14 -0800

    Added basic interface layout and styling.

---
+++ src/interface.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+  <head>
+    <!--
+      CS290 PHP MySQL Assignment
+      Author: Scott Milton
+      Date: 2/15/15
+      Description:  This project is a simple interface to work with a database that tracks a store's inventory of videos.
+    -->
+    <meta charset="UTF-8">
+
+    <title>
+      CS 290 PHP Assignment 2
+    </title>
+
+    <link href="style.css" rel="stylesheet">
+    <script src="code.js"></script>
+
+  </head>
+  <body>
+    <section>
+      <h1>
+        Video Store Inventory System
+      </h1>
+
+      <form action="#" method="post">
+        <fieldset>
+          <p>
+            <button id="submit-button" type="button">
+              Add video
+            </button>
+          </p>
+        </fieldset>
+      </form>
+
+      <table id="video-list">
+        <tr>
+          <th>status
+          <th>manage
+          <th>category
+          <th>length
+        </tr>
+        <tr>
+          <td>
+          <td>
+          <td>
+          <td>
+      </table>
+
+    </section>
+  </body>
+</html>
